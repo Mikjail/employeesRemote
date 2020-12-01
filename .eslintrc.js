10cf5d65ca496c867aa0a9e9860ecf51cfbc6cdb
Mikjail Salazar <mikjailsalazar@gmail.com>
2020-12-01 15:49:37 +0400

    App - Adjusting test and style

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,5 +15,6 @@ module.exports = {
   rules: {
     'import/prefer-default-export': 'off',
     'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
+    'comma-dangle': ['error', 'never'],
   },
 };
